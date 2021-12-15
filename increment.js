8488535548e94662e44fd8a9d21329c322497e38
chrisp9974 <92195214+chrisp9974@users.noreply.github.com>
2021-12-14 19:27:08 -0800

    saved count with localStorage

--- increment.js
+++ increment.js
@@ -1,7 +1,18 @@
 function incrementNum() {
-    var element = document.getElementById('number');
-    var value = element.innerHTML;
+    // var element = document.getElementById('number');
+    // var value = element.innerHTML;
 
-    ++value;
-    document.getElementById('number').innerHTML = value;
+    // ++value;
+    // document.getElementById('number').innerHTML = value;
+
+    if (localStorage.getItem('count') === null) {
+        document.querySelector('#number').innerHTML = 0;
+    }
+    else {
+        document.querySelector('#number').innerHTML = Number(localStorage.getItem('count'));
+        let value = document.querySelector('#number').innerHTML;
+        value++;
+        localStorage.setItem('count', value.toString());
+        document.querySelector('#number').innerHTML = Number(localStorage.getItem('count'));
+    }
 }
