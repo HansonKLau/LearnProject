f9476df6762e9cba3a8d0219799c6c29530366c2
chrisp9974 <92195214+chrisp9974@users.noreply.github.com>
2021-12-17 14:39:24 -0800

    initializing firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello from index.js');
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqZhh7ipuEznMRgA5Sn-9Pk3nt4W6Z9s4",
+    authDomain: "learnproject-ed665.firebaseapp.com",
+    projectId: "learnproject-ed665",
+    storageBucket: "learnproject-ed665.appspot.com",
+    messagingSenderId: "310311127746",
+    appId: "1:310311127746:web:84e2786c6c29d5bc75095b"
+};
+
+initializeApp(firebaseConfig);
