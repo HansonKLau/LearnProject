b42465f3ef8963d789769871fcd503e4f8400d72
chrisp9974 <92195214+chrisp9974@users.noreply.github.com>
2021-12-17 16:57:30 -0800

    retrieving data from firebase

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBqZhh7ipuEznMRgA5Sn-9Pk3nt4W6Z9s4",
@@ -9,4 +12,25 @@ const firebaseConfig = {
     appId: "1:310311127746:web:84e2786c6c29d5bc75095b"
 };
 
+// init firebase app
 initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection reference
+const colRef = collection(db, 'accounts');
+
+// get collection data
+getDocs(colRef)
+    .then((snapshot) => {
+        let accounts = [];
+        snapshot.docs.forEach((doc) => {
+            // ... is the spread operator, it breaks doc.data() down into individual elements
+            accounts.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(accounts);
+    })
+    .catch((e) => {
+        console.error(e.message);
+    })
